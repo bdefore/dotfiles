626f7ee59f51c010e6270ddeb90ffde8b51e66de
Buck DeFore <bdefore@gmail.com>
2021-10-10 09:35:07 -0400

    pacman mirror test, some default firefox settings, bind audio keys

--- private_dot_mozilla/private_firefox/private_4p5dih9t.default-release/user.js
+++ private_dot_mozilla/private_firefox/private_4p5dih9t.default-release/user.js
@@ -1,2 +1,9 @@
+// disable slow gui tab switcher
 user_pref("browser.ctrlTab.sortByRecentlyUsed", false);
+
+// add-on for creating pwas, hide top chroming unnecessary for sway
 user_pref("firefoxpwa.enableHidingIconBar", true);
+
+// disable password saving
+user_pref("signon.passwordEditCapture.enabled", false);	
+user_pref("services.sync.engine.passwords", false);
